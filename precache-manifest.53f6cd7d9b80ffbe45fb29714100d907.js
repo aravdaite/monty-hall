63dc0f713d3e26b51f76e308b19265562b29e802
aravdaite <aravdaite@gmail.com>
2020-04-02 20:40:26 +0200

    Updates

---
+++ precache-manifest.53f6cd7d9b80ffbe45fb29714100d907.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcacb0fc56aca3c9513249cd75dbe3c4",
+    "url": "/monty-hall/index.html"
+  },
+  {
+    "revision": "b6e10e2d08c3c04860ec",
+    "url": "/monty-hall/static/css/main.487d4638.chunk.css"
+  },
+  {
+    "revision": "39905e3c7b2aeeb084b2",
+    "url": "/monty-hall/static/js/2.0ca04766.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/monty-hall/static/js/2.0ca04766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6e10e2d08c3c04860ec",
+    "url": "/monty-hall/static/js/main.f3ea8306.chunk.js"
+  },
+  {
+    "revision": "564345f2d6e9f67984bc",
+    "url": "/monty-hall/static/js/runtime-main.144e9297.js"
+  }
+]);
